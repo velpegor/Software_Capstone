85871b53b6b41e9abb9dedce83e63318b3171725
Wonjun Lee <83739271+velpegor@users.noreply.github.com>
2022-11-05 22:17:45 +0900

    주석 추가

--- deface_auto.py
+++ deface_auto.py
@@ -1,11 +1,13 @@
 import os
 import pathlib
 
+#WSL Ubuntu 환경에서 pydeface 자동화
+#Pydeface, FSL 설치 필요
+
 filename = []
-data_root = pathlib.Path('/mnt/e/deface/IXI-T1/')
+data_root = pathlib.Path('/mnt/e/deface/IXI-T1/') #경로 설정
 for item in data_root.iterdir():
-  filename.append(os.path.basename(item))
+  filename.append(os.path.basename(item)) #파일명만 추출
 
 for i in range(len(filename)):
-    os.system('pydeface {}'.format(filename[i]))
-
+    os.system('pydeface {}'.format(filename[i]))  #pydeface 실행
